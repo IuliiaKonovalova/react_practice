b0a8f8e272913aa63ff07533137d729d2eb21cd8
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-29 15:44:49 +0700

    Add images and fix styling cards

--- monsters-rolodex/src/components/CardList.js
+++ monsters-rolodex/src/components/CardList.js
@@ -4,10 +4,28 @@ const CardList = (props) => {
   const monstersList = props.monsters
   console.log("CardList" + monstersList);
   return (
-    <div>
-      {monstersList && monstersList.map((monster) => (
-        <h2 key={monster.id}>{monster.name}</h2>
-      ))}
+    <div
+      className="p-5 my-7 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {monstersList && monstersList.map((monster) => (
+          <div
+            key={monster.id}
+            className="bg-white shadow-md rounded-lg p-4">
+              <h2
+                className="text-2xl text-lime-700 font-bold mb-2">
+                  {monster.name}
+              </h2>
+              <div className="flex justify-center mb-5">
+
+                <img
+                  className="w-32 h-32 rounded-full mx-auto"
+                  alt={monster.name}
+                  src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+              </div>
+              <p className="text-[#b45309]">
+                {monster.email}
+              </p>
+          </div>
+        ))}
     </div>
   );
 }
