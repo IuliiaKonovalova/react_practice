375e19e71772fdb7d8eeed55ba7ec49cd8042e2d
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-29 15:45:10 +0700

    Add styles to search bar

--- monsters-rolodex/src/components/SearchMonstersBox.js
+++ monsters-rolodex/src/components/SearchMonstersBox.js
@@ -7,6 +7,7 @@ const SearchMonstersBox = (props) => {
 
   return (
     <input
+      className="my-3 p-2 rounded-lg border-2 border-lime-500 focus:outline-none focus:border-lime-600"
       type="search"
       placeholder={props.placeholder}
       onChange={onChange}
